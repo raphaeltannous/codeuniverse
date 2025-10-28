2f6a3f1aba24773f50e2fbffcbe1ec4573cab149
Raphael Tannous <raphaeltannous@proton.me>
2025-10-28 21:18:11 +0200

    Creating a server with graceful shutdown

---
+++ cmd/server/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"log"
+	"net/http"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	server := &http.Server{Addr: ":3333", Handler: service()}
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatal(err)
+		}
+	}()
+
+	<-ctx.Done()
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func service() http.Handler {
+	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Logger)
+
+	// todo
+
+	return r
+}
--- go.mod
+++ go.mod
@@ -6,3 +6,5 @@ ignore (
 	./.database
 	./frontend
 )
+
+require github.com/go-chi/chi/v5 v5.2.3
