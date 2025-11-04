26d864ac9f0d030cf8887cd39daa4efcac7d7703
Raphael Tannous <raphaeltannous@proton.me>
2025-11-04 21:16:46 +0200

    Updating user profile and creating user profile interface

--- internal/models/user_profile.go
+++ internal/models/user_profile.go
@@ -15,19 +15,19 @@ type UserProfile struct {
 	Country           *string `db:"country" json:"country"`
 	PreferredLanguage *string `db:"preferred_language" json:"preferredLanguage"`
 
-	TotalSubmissions    *int `db:"total_submissions" json:"totalSubmissions"`
-	AcceptedSubmissions *int `db:"accepted_submissions" json:"acceptedSubmissions"`
-	ProblemsSolved      *int `db:"problems_solved" json:"problemsSolved"`
-	EasySolved          *int `db:"easy_solved" json:"easySolved"`
-	MediumSolved        *int `db:"medium_solved" json:"mediumSolved"`
-	HardSolved          *int `db:"hard_solved" json:"hardSolved"`
+	TotalSubmissions    int `db:"total_submissions" json:"totalSubmissions"`
+	AcceptedSubmissions int `db:"accepted_submissions" json:"acceptedSubmissions"`
+	ProblemsSolved      int `db:"problems_solved" json:"problemsSolved"`
+	EasySolved          int `db:"easy_solved" json:"easySolved"`
+	MediumSolved        int `db:"medium_solved" json:"mediumSolved"`
+	HardSolved          int `db:"hard_solved" json:"hardSolved"`
 
 	WebsiteURL  *string `db:"website_url" json:"websiteUrl"`
 	GithubURL   *string `db:"github_url" json:"githubUrl"`
 	LinkedinURL *string `db:"linkedin_url" json:"linkedinUrl"`
 	XURL        *string `db:"x_url" json:"xUrl"`
 
-	LastActive *time.Time `db:"last_active" json:"lastActive"`
-	CreatedAt  time.Time  `db:"created_at" json:"createdAt"`
-	UpdatedAt  time.Time  `db:"updated_at" json:"updatedAt"`
+	LastActive time.Time `db:"last_active" json:"lastActive"`
+	CreatedAt  time.Time `db:"created_at" json:"createdAt"`
+	UpdatedAt  time.Time `db:"updated_at" json:"updatedAt"`
 }
