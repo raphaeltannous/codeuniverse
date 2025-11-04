fe0a40a383bb0581d2b9cb44314ce4dd7d4107bb
Raphael Tannous <raphaeltannous@proton.me>
2025-11-04 20:02:45 +0200

    Refactoring some migrations

--- migrations/00001_users.sql
+++ migrations/00001_users.sql
@@ -2,12 +2,15 @@
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+
     username VARCHAR(50) UNIQUE NOT NULL,
     password_hash TEXT NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     email_verified BOOLEAN DEFAULT false,
+
     is_active BOOLEAN DEFAULT true,
     role VARCHAR(20) DEFAULT 'user',
+
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
--- migrations/00006_making_problem_number_unique.sql
+++ migrations/00006_making_problem_number_unique.sql
@@ -1,11 +1,11 @@
 -- +goose Up
 -- +goose StatementBegin
-ALTER TABLE IF EXISTS problems
+ALTER TABLE problems
 ADD CONSTRAINT unique_number UNIQUE (number);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-ALTER TABLE IF EXISTS problems
+ALTER TABLE problems
 DROP CONSTRAINT unique_number;
 -- +goose StatementEnd
