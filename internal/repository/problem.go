845ed5470267a8bfead6b7f407098ccf28d1fc3e
Raphael Tannous <raphaeltannous@proton.me>
2025-11-04 14:07:47 +0200

    Updating problem repository interface

--- internal/repository/problem.go
+++ internal/repository/problem.go
@@ -9,39 +9,44 @@ import (
 )
 
 type ProblemRepository interface {
-	GetAllProblems(ctx context.Context, limit, offset int) ([]*models.Problem, error)
+	GetProblems(ctx context.Context, limit, offset int) ([]*models.Problem, error)
 
 	Create(ctx context.Context, problem *models.Problem) (uuid.UUID, error)
 	Delete(ctx context.Context, id uuid.UUID) error
 
 	GetByID(ctx context.Context, id uuid.UUID) (*models.Problem, error)
 	GetBySlug(ctx context.Context, slug string) (*models.Problem, error)
+	GetByNumber(ctx context.Context, number int) (*models.Problem, error)
 
-	UpdateTitle(ctx context.Context, title string) error
-	UpdateSlug(ctx context.Context, slug string) error
+	UpdateTitle(ctx context.Context, id uuid.UUID, title string) error
+	UpdateSlug(ctx context.Context, id uuid.UUID, slug string) error
 
-	UpdateDescription(ctx context.Context, descripton string) error
+	UpdateDescription(ctx context.Context, id uuid.UUID, description string) error
 
-	UpdateDifficulty(ctx context.Context, difficulty string) error
+	UpdateDifficulty(ctx context.Context, id uuid.UUID, difficulty string) error
 
-	AddTags(ctx context.Context, tags []string) error
-	AddTag(ctx context.Context, tag string) error
-	RemoveTag(ctx context.Context, tag string) error
+	AddTags(ctx context.Context, id uuid.UUID, tags []string) error
+	AddTag(ctx context.Context, id uuid.UUID, tag string) error
+	RemoveTag(ctx context.Context, id uuid.UUID, tag string) error
 
-	AddHints(ctx context.Context, hints []string) error
-	AddHint(ctx context.Context, hint string) error
-	RemoveHint(ctx context.Context, hint string) error
+	AddHints(ctx context.Context, id uuid.UUID, hints []string) error
+	AddHint(ctx context.Context, id uuid.UUID, hint string) error
+	RemoveHint(ctx context.Context, id uuid.UUID, hint string) error
 
-	UpdateCodeSnippets(ctx context.Context, codeSnippets string) error
-	UpdateTestcases(ctx context.Context, testCases string) error
+	UpdateCodeSnippets(ctx context.Context, id uuid.UUID, codeSnippets string) error
+	UpdateTestcases(ctx context.Context, id uuid.UUID, testCases string) error
 
 	UpdatePublic(ctx context.Context, id uuid.UUID, status bool) error
-	UpdatePaind(ctx context.Context, id uuid.UUID, status bool) error
+	UpdatePaid(ctx context.Context, id uuid.UUID, status bool) error
 
 	IncrementLikes(ctx context.Context, id uuid.UUID, incrementValue int) error
 	DecrementLikes(ctx context.Context, id uuid.UUID, decrementValue int) error
 	IncrementDislikes(ctx context.Context, id uuid.UUID, incrementValue int) error
 	DecrementDislikes(ctx context.Context, id uuid.UUID, decrementValue int) error
+
+	// TODO:
+	// - Search capabilities.
+	// - Get all problems?
 }
 
 var (
