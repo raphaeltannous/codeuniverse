9a4d1e4158c3b383bd488bbea5558cd64a9efb25
Raphael Tannous <raphaeltannous@proton.me>
2025-11-08 15:14:28 +0200

    Implementing the authentication middleware

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -1,13 +1,52 @@
 package middleware
 
 import (
-	"fmt"
+	"context"
 	"net/http"
+	"strings"
+
+	"git.riyt.dev/codeuniverse/internal/services"
+	"git.riyt.dev/codeuniverse/internal/utils"
+	"github.com/golang-jwt/jwt/v5"
 )
 
-func AuthMiddleware(next http.Handler) http.Handler {
+const UserCtxKey = "user"
+
+func AuthMiddleware(next http.Handler, userService services.UserService) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("auth middleware")
-		next.ServeHTTP(w, r)
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer ") {
+			http.Error(w, "missing token", http.StatusUnauthorized)
+			return
+		}
+
+		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+		token, err := utils.ValidateJWT(tokenString)
+		if err != nil {
+			http.Error(w, "invalid token", http.StatusUnauthorized)
+			return
+		}
+
+		if !token.Valid {
+			http.Error(w, "invalid or expired token", http.StatusUnauthorized)
+			return
+		}
+
+		claims, ok := token.Claims.(jwt.MapClaims)
+		if !ok {
+			http.Error(w, "invalid claims", http.StatusUnauthorized)
+			return
+		}
+
+		username := claims["username"].(string)
+		user, err := userService.GetByUsername(r.Context(), username)
+		if err != nil {
+			http.Error(w, "username not found", http.StatusUnauthorized)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), UserCtxKey, user)
+
+		next.ServeHTTP(w, r.WithContext(ctx))
 	})
 }
