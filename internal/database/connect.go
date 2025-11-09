80fb91a60014e7895ae22701ec631a7a9f963bbf
Raphael Tannous <raphaeltannous@proton.me>
2025-11-09 21:39:30 +0200

    Checking if the db string is empty and erroring if it is

--- internal/database/connect.go
+++ internal/database/connect.go
@@ -2,14 +2,23 @@ package database
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"os"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
+var (
+	ErrEmptyDBString = errors.New("db string is empty")
+)
+
 func Connect() (*sql.DB, error) {
 	connectionString := os.Getenv("CODEUNIVERSE_DBSTRING")
+	if connectionString == "" {
+		return nil, ErrEmptyDBString
+	}
+
 	db, err := sql.Open("pgx", connectionString)
 	if err != nil {
 		return nil, fmt.Errorf("failed to make a connection to the datbase: %w", err)
