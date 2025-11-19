b4a5765400f571d1c74aac06735f44ed7fa464bd
Raphael Tannous <raphaeltannous@proton.me>
2025-11-19 23:21:19 +0200

    Updating mfa code model

--- internal/models/mfa_code.go
+++ internal/models/mfa_code.go
@@ -9,8 +9,9 @@ import (
 type MfaCode struct {
 	ID uuid.UUID `db:"id" json:"-"`
 
-	UserId uuid.UUID `db:"user_id" json:"-"`
-	Hash   string    `db:"code_hash" json:"-"`
+	UserId    uuid.UUID `db:"user_id" json:"-"`
+	TokenHash string    `db:"token_hash" json:"-"`
+	CodeHash  string    `db:"code_hash" json:"-"`
 
 	ExpiresAt time.Time `db:"expires_at" json:"-"`
 	CreatedAt time.Time `db:"created_at" json:"-"`
