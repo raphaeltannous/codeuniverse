7c1a43599ad1384aa23c9a293966bc0594392a04
Raphael Tannous <raphaeltannous@proton.me>
2025-11-20 17:42:11 +0200

    Creating LogValue method for mfaCode

--- internal/models/mfa_code.go
+++ internal/models/mfa_code.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"log/slog"
 	"time"
 
 	"github.com/google/uuid"
@@ -15,4 +16,17 @@ type MfaCode struct {
 
 	ExpiresAt time.Time `db:"expires_at" json:"-"`
 	CreatedAt time.Time `db:"created_at" json:"-"`
+}
+
+func (mc MfaCode) LogValue() slog.Value {
+	return slog.GroupValue(
+		slog.String("ID", mc.ID.String()),
+
+		slog.String("UserId", mc.UserId.String()),
+		slog.String("TokenHash", mc.TokenHash),
+		slog.String("CodeHash", mc.CodeHash),
+
+		slog.Time("ExpiresAt", mc.ExpiresAt),
+		slog.Time("CreatedAt", mc.CreatedAt),
+	)
 }
