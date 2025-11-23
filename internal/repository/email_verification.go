6d238d496030cac035ef6e6942223aedca913df8
Raphael Tannous <raphaeltannous@proton.me>
2025-11-23 14:45:13 +0200

    Updating email_verification repository

--- internal/repository/email_verification.go
+++ internal/repository/email_verification.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"context"
+	"errors"
 	"time"
 
 	"git.riyt.dev/codeuniverse/internal/models"
@@ -11,5 +12,9 @@ import (
 type EmailVerificationRepository interface {
 	Save(ctx context.Context, userId uuid.UUID, hash string, expiresAt time.Time) error
 
-	GetByTokenHash(ctx context.Context, hash string) (emailVerification *models.EmailVerification, err error)
+	GetByTokenHash(ctx context.Context, hash string) (*models.EmailVerification, error)
 }
+
+var (
+	ErrEmailVerificationNotFound = errors.New("postgres: email verification not found")
+)
