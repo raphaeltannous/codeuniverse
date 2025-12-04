52f822a9d8f4b81c89f5eef5b050e7778070c585
Raphael Tannous <raphaeltannous@proton.me>
2025-12-04 15:28:57 +0200

    Implementing the submission repository

--- internal/repository/postgres/submission.go
+++ internal/repository/postgres/submission.go
@@ -1,1 +1,133 @@
 package postgres
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+
+	"git.riyt.dev/codeuniverse/internal/models"
+	"git.riyt.dev/codeuniverse/internal/repository"
+	"github.com/google/uuid"
+)
+
+type postgresSubmissionRepository struct {
+	db *sql.DB
+}
+
+func (p *postgresSubmissionRepository) Create(ctx context.Context, submission *models.Submission) (*models.Submission, error) {
+	query := `
+		INSERT INTO
+			submissions (user_id, problem_id, language, code, status)
+		VALUES
+			($1, $2, $3, $4, $5)
+		RETURNING
+			id;
+	`
+
+	row := p.db.QueryRowContext(
+		ctx,
+		query,
+		submission.UserId,
+		submission.ProblemId,
+		submission.Language,
+		submission.Code,
+		submission.Status,
+	)
+
+	err := row.Scan(&submission.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	return submission, nil
+}
+
+func (p *postgresSubmissionRepository) Delete(ctx context.Context, id uuid.UUID) error {
+	panic("unimplemented")
+}
+
+func (p *postgresSubmissionRepository) UpdateAcceptanceStatus(ctx context.Context, id uuid.UUID, status bool) error {
+	return p.updateColumnValue(
+		ctx,
+		id,
+		"is_accepted",
+		status,
+	)
+}
+
+func (p *postgresSubmissionRepository) UpdateExecutionTime(ctx context.Context, id uuid.UUID, executionTime float64) error {
+	return p.updateColumnValue(
+		ctx,
+		id,
+		"execution_time",
+		executionTime,
+	)
+}
+
+func (p *postgresSubmissionRepository) UpdateMemoryUsage(ctx context.Context, id uuid.UUID, memoryUsage float64) error {
+	return p.updateColumnValue(
+		ctx,
+		id,
+		"memory_usage",
+		memoryUsage,
+	)
+}
+
+func (p *postgresSubmissionRepository) UpdateStatus(ctx context.Context, id uuid.UUID, status string) error {
+	return p.updateColumnValue(
+		ctx,
+		id,
+		"status",
+		status,
+	)
+}
+
+func (p *postgresSubmissionRepository) GetById(ctx context.Context, id uuid.UUID) (*models.Submission, error) {
+	query := `
+		SELECT id, status, execution_time, memory_usage, is_accepted
+		FROM submissions
+		WHERE id = $1;
+	`
+
+	row := p.db.QueryRowContext(
+		ctx,
+		query,
+		id,
+	)
+
+	submission := new(models.Submission)
+	if err := p.scanSubmissionFunc(row, submission); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, repository.ErrSubmissionNotFound
+		}
+
+		return nil, repository.ErrInternalServerError
+	}
+
+	return submission, nil
+}
+
+func (p *postgresSubmissionRepository) GetProblemSubmissions(ctx context.Context, userId uuid.UUID, problemId uuid.UUID) ([]*models.Submission, error) {
+	panic("unimplemented")
+}
+
+func (p *postgresSubmissionRepository) updateColumnValue(ctx context.Context, id uuid.UUID, column string, value any) error {
+	return updateColumnValue(
+		ctx,
+		p.db,
+		"submissions",
+		id,
+		column,
+		value,
+	)
+}
+
+func (p *postgresSubmissionRepository) scanSubmissionFunc(scanner postgresScanner, submission *models.Submission) error {
+	return scanner.Scan(
+		&submission.ID,
+	)
+}
+
+func NewSubmissionRepository(db *sql.DB) repository.SubmissionRepository {
+	return &postgresSubmissionRepository{db: db}
+}
--- internal/repository/submission.go
+++ internal/repository/submission.go
@@ -12,6 +12,11 @@ type SubmissionRepository interface {
 	Create(ctx context.Context, submission *models.Submission) (*models.Submission, error)
 	Delete(ctx context.Context, id uuid.UUID) error
 
+	UpdateStatus(ctx context.Context, id uuid.UUID, status string) error
+	UpdateExecutionTime(ctx context.Context, id uuid.UUID, executionTime float64) error
+	UpdateMemoryUsage(ctx context.Context, id uuid.UUID, memoryUsage float64) error
+	UpdateAcceptanceStatus(ctx context.Context, id uuid.UUID, status bool) error
+
 	GetProblemSubmissions(ctx context.Context, userId, problemId uuid.UUID) ([]*models.Submission, error)
 
 	GetById(ctx context.Context, id uuid.UUID) (*models.Submission, error)
