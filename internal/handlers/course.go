afa3f93725b471903afbccad10eb5e2c7536d8bd
Raphael Tannous <raphaeltannous@proton.me>
2025-12-25 17:48:25 +0200

    Setting up course handlers

--- internal/handlers/course.go
+++ internal/handlers/course.go
@@ -1,1 +1,13 @@
 package handlers
+
+import "git.riyt.dev/codeuniverse/internal/services"
+
+type CourseHandler struct {
+	courseService services.CourseService
+}
+
+func NewCourseHandler(courseService services.CourseService) *CourseHandler {
+	return &CourseHandler{
+		courseService: courseService,
+	}
+}
