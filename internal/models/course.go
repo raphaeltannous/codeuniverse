0ff36533e528a03120c875eb771309c3aa12807d
Raphael Tannous <raphaeltannous@proton.me>
2025-12-25 17:46:08 +0200

    Updating Course model

--- internal/models/course.go
+++ internal/models/course.go
@@ -10,8 +10,12 @@ type Course struct {
 	ID uuid.UUID `db:"id" json:"id"`
 
 	Title        string `db:"title" json:"title"`
+	Slug         string `db:"slug" json:"slug"`
 	Description  string `db:"title" json:"description"`
 	ThumbnailURL string `db:"thumbnail_url" json:"thumbnailURL"`
+	Difficulty   string `db:"difficulty" json:"difficulty"`
+
+	TotalLessons string `json:"totalLessons"`
 
 	CreatedAt time.Time `db:"created_at" json:"createdAt"`
 	UpdatedAt time.Time `db:"updated_at" json:"updatedAt"`
