03306298b102edb0cec12d78e7c07d4606be062e
Raphael Tannous <raphaeltannous@proton.me>
2026-01-10 20:20:37 +0200

    Moving RunResult to another file

--- internal/models/run.go
+++ internal/models/run.go
@@ -45,30 +45,3 @@ func NewRun(
 		Status:   status,
 	}
 }
-
-type RunResult struct {
-	Status ResultStatus
-
-	FailedTestcases []*FailedTestcase
-
-	MemoryUsage   float64
-	ExecutionTime float64
-
-	StdOut string
-	StdErr string
-}
-
-func NewRunResult(results *Results) *RunResult {
-	runResults := &RunResult{
-		FailedTestcases: results.FailedTestcases,
-		ExecutionTime:   results.ExecutionTime,
-	}
-
-	if results.IsPassed {
-		runResults.Status = StatusAccepted
-	} else {
-		runResults.Status = StatusFailed
-	}
-
-	return runResults
-}
