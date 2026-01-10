71bb1280f27a1ca6f24eb0df0c6139357c96b6a5
Raphael Tannous <raphaeltannous@proton.me>
2026-01-10 20:13:08 +0200

    Updating ResultStatus and creating FailedTestcase type

--- internal/models/result_status.go
+++ internal/models/result_status.go
@@ -1,6 +1,24 @@
 package models
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
+
+type Results struct {
+	Exeption        string            `json:"exception"`
+	IsPassed        bool              `json:"isPassed"`
+	FailedTestcases []*FailedTestcase `json:"failedTestcases"`
+
+	ExecutionTime float64 `json:"executionTime"`
+}
+
+type FailedTestcase struct {
+	Id       int `json:"id"`
+	Input    any `json:"input"`
+	Expected any `json:"expected"`
+	Got      any `json:"got"`
+}
 
 type ResultStatus int
 
@@ -19,6 +37,40 @@ const (
 
 	StatusInternalServerError
 )
+
+var (
+	ErrInvalidResultStatus = errors.New("invalid result status")
+)
+
+func ParseResultStatus(status string) (ResultStatus, error) {
+	switch status {
+	case "Pending":
+		return StatusPending, nil
+	case "Started":
+		return StatusStarted, nil
+
+	case "Accepted":
+		return StatusAccepted, nil
+	case "Failed":
+		return StatusFailed, nil
+
+	case "Time Limit Exceeded":
+		return StatusTimeLimitExceeded, nil
+	case "Memory Limit Exceeded":
+		return StatusMemoryLimitExceeded, nil
+
+	case "Compile Error":
+		return StatusCompileError, nil
+	case "Runtime Error":
+		return StatusRuntimeError, nil
+
+	case "Internal Server Error":
+		return StatusInternalServerError, nil
+
+	default:
+		return 0, ErrInvalidResultStatus
+	}
+}
 
 func (r ResultStatus) String() string {
 	statuses := map[ResultStatus]string{
