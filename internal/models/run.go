91dbcd58e647f58fdab65137ff1366f7f6a76a4c
Raphael Tannous <raphaeltannous@proton.me>
2026-01-10 20:14:35 +0200

    Updating RunResult and Run

--- internal/models/run.go
+++ internal/models/run.go
@@ -12,24 +12,20 @@ type Run struct {
 	UserId    uuid.UUID `db:"user_id" json:"-"`
 	ProblemId uuid.UUID `db:"problem_id" json:"-"`
 
-	Language string `db:"language" json:"language"`
-	Code     string `db:"code" json:"code"`
-	Status   string `db:"status" json:"status"`
+	Language string       `db:"language" json:"language"`
+	Code     string       `db:"code" json:"code"`
+	Status   ResultStatus `db:"status" json:"status"`
 
 	ExecutionTime float64 `db:"execution_time" json:"executionTime"`
 	MemoryUsage   float64 `db:"memory_usage" json:"memoryUsage"`
 
-	IsAccepted bool `db:"is_accepted" json:"isAccepted"`
+	FailedTestcases []*FailedTestcase `db:"failed_testcases" json:"failedTestcases"`
+
+	StdOut string `db:"stdout" json:"stdOut"`
+	StdErr string `db:"sterr" json:"stdErr"`
 
 	CreatedAt time.Time `db:"created_at" json:"createdAt"`
 	UpdatedAt time.Time `db:"updated_at" json:"updatedAt"`
-}
-
-type RunResult struct {
-	Status   ResultStatus
-	Input    any
-	Expected any
-	Got      any
 }
 
 func NewRun(
@@ -38,7 +34,7 @@ func NewRun(
 
 	language string,
 	code string,
-	status string,
+	status ResultStatus,
 ) *Run {
 	return &Run{
 		UserId:    userId,
@@ -48,4 +44,31 @@ func NewRun(
 		Code:     code,
 		Status:   status,
 	}
+}
+
+type RunResult struct {
+	Status ResultStatus
+
+	FailedTestcases []*FailedTestcase
+
+	MemoryUsage   float64
+	ExecutionTime float64
+
+	StdOut string
+	StdErr string
+}
+
+func NewRunResult(results *Results) *RunResult {
+	runResults := &RunResult{
+		FailedTestcases: results.FailedTestcases,
+		ExecutionTime:   results.ExecutionTime,
+	}
+
+	if results.IsPassed {
+		runResults.Status = StatusAccepted
+	} else {
+		runResults.Status = StatusFailed
+	}
+
+	return runResults
 }
