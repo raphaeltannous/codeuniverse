42a08a4c7f83dec176ba743029f55b95f65300f1
Raphael Tannous <raphaeltannous@proton.me>
2026-01-14 13:55:59 +0200

    Comment sleep in router.go

--- internal/router/router.go
+++ internal/router/router.go
@@ -4,7 +4,6 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
-	"time"
 
 	"git.riyt.dev/codeuniverse/internal/handlers"
 	"github.com/go-chi/chi/v5"
@@ -39,12 +38,12 @@ func Service(
 
 	r.Use(chimiddleware.RequestID)
 	r.Use(chimiddleware.Logger)
-	r.Use(func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			time.Sleep(1 * time.Second)
-			next.ServeHTTP(w, r)
-		})
-	})
+	// r.Use(func(next http.Handler) http.Handler {
+	// 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	// 		time.Sleep(1 * time.Second)
+	// 		next.ServeHTTP(w, r)
+	// 	})
+	// })
 
 	r.Mount("/api", apiRouter(
 		userHandler,
