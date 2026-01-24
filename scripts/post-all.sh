818a3487b3e313f344f8c08df9e6e3084a53d08e
Raphael Tannous <raphaeltannous@proton.me>
2026-01-24 12:42:57 +0200

    Reumoving scripts

--- scripts/post-all.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-for userData in data/*.json; do
-  curl -X POST localhost:3333/api/auth/signup \
-    -H "Content-Type: application/json" \
-    --data-binary "@$userData"
-done
--- scripts/post-single.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-curl -X POST "localhost:3333$1" \
-  -H "Content-Type: application/json" \
-  --data-binary "@$2"
